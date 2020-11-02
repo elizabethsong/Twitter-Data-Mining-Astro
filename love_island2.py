381636bf913032ea8afbb65f73a5e8ffb78e63e2
Elizabeth <elizabethsong65@yahoo.com>
2020-11-02 17:00:45 -0500

    data table

--- love_island2.py
+++ love_island2.py
@@ -9,6 +9,8 @@ import seaborn as sns
 import praw
 import redditdetails
 
+#tutorial: https://www.learndatasci.com/tutorials/sentiment-analysis-reddit-headlines-pythons-nltk/
+
 reddit = praw.Reddit(client_id=redditdetails.client_id,
                      client_secret=redditdetails.client_secret,
                      user_agent=redditdetails.username)
@@ -16,9 +18,10 @@ sns.set(style='darkgrid', context='talk', palette='Dark2')
 
 headlines = set()
 
-for submission in reddit.subreddit('loveislandUSA').new(limit=None):
+for submission in reddit.subreddit('thebachelor').new(limit=None):
     headlines.add(submission.title)
     display.clear_output()
+    #print(str(len(headlines)) + " " + submission.title)
     print(len(headlines))
 import nltk
 nltk.download("vader_lexicon")
@@ -32,7 +35,7 @@ for line in headlines:
     pol_score['headline'] = line
     results.append(pol_score)
 
-pprint(results[:3], width=100)
+pprint(results[:10], width=100)
 
 df = pd.DataFrame.from_records(results)
-df.head()
+print(df.head(20))
